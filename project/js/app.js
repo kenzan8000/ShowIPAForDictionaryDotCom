d12601ecd5bc02f2c219aabd65d9921f4ace96b2
kenzan8000 <kenzan8000@gmail.com>
2014-08-19 20:23:05 +0900

    fix bugs because dictionary.com changed dom

--- project/js/app.js
+++ project/js/app.js
@@ -1,18 +1,19 @@
 (function() {
-    // delete pron_toggle
-    var toggleLinks = document.getElementsByClassName('pron_toggle');
+    // clear toggle
+    var toggleLinks = document.getElementsByClassName('pronounce-IPA-button');
     for (var i = 0; i < toggleLinks.length; i++) {
-        toggleLinks[i].innerHTML = '';
+        toggleLinks[i].style.display = 'none';
     }
 
-    // show IPA
-    var spans = document.getElementsByTagName("span");
-    for (var i = 0; i < spans.length; i++) {
-        var classNameOfSpan = spans[i].className;
-        if (classNameOfSpan == "show_ipapr"   ||
-            classNameOfSpan == "show_spellpr" ||
-            classNameOfSpan == "show_rhpr") {
-            spans[i].style.display="inline";
-        }
+    // show Spell and IPA
+    var spellPronunciations = document.getElementsByClassName('spellpron');
+    for (var i = 0; i < spellPronunciations.length; i++) {
+        spellPronunciations[i].style.display = "inline";
+        spellPronunciations[i].className = "";
+    }
+    var IPAPronunciations = document.getElementsByClassName('ipapron');
+    for (var i = 0; i < IPAPronunciations.length; i++) {
+        IPAPronunciations[i].style.display = "inline";
+        IPAPronunciations[i].className = "";
     }
 })();
